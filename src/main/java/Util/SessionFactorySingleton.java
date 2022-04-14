be4a0f87c95c026b64848da34f98afa7cc2287c5
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-04-14 12:53:05 +0430

    create SessionFactorySingleton

---
+++ src/main/java/Util/SessionFactorySingleton.java
@@ -0,0 +1,39 @@
+package Util;
+
+import Entity.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SessionFactorySingleton {
+
+    private SessionFactorySingleton() {}
+
+    private static class LazyHolder {
+        static SessionFactory INSTANCE;
+
+        static {
+            var registry = new StandardServiceRegistryBuilder()
+                    .configure()
+                    .build();
+
+            INSTANCE = new MetadataSources(registry)
+                    .addAnnotatedClass(Person.class)
+                    .addAnnotatedClass(Course.class)
+                    .addAnnotatedClass(Professor.class)
+                    .addAnnotatedClass(Student.class)
+                    .addAnnotatedClass(AllCourse.class)
+                    .addAnnotatedClass(Employee.class)
+                    .addAnnotatedClass(StatusEmployee.class)
+                    .addAnnotatedClass(GradeCourse.class)
+                    .addAnnotatedClass(ChooseUnit.class)
+
+                    .buildMetadata()
+                    .buildSessionFactory();
+        }
+    }
+
+    public static SessionFactory getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+}
